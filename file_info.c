1db5c309ae327f4d5f27c73b4351d38a51b1002e
DragosChileban <andreidragos029@gmail.com>
2023-03-28 09:54:56 +0300

    Saptamana 6

--- file_info.c
+++ file_info.c
@@ -3,23 +3,60 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include<string.h>
+ 
+ 
 int main(int argc, char * argv[]) {
-
+ 
     struct stat inf;
-
-    printf("%d arguments\n", argc);
-
+    char choice[10];
+ 
+    // printf("%d arguments\n", argc);
+ 
     for(int i = 1; i < argc; i++) {
+        printf("Current file:  %s is a ", argv[i]);
         if(lstat(argv[i],&inf) == -1) {
-            exit(EXIT_FAILURE);
             perror("lstat() error");
+            exit(EXIT_FAILURE);
         }
-        printf("%s\n", argv[i]);
+ 
+        if (S_ISREG(inf.st_mode)) {
+            printf("regular file\n");
+            printf("Menu for regular file \n");
+            printf("1. Read -n \n");
+            printf("2. Size -d \n");
+            printf("3. Number of hard links -h \n");
+            printf("4. Time of last modification -m \n");
+            printf("5. Access rights -a \n");
+            printf("6. Create a symbolic link give:link name -l \n");
+            scanf("%s", choice);
+            for(int i = 1; i < strlen(choice); i++) {
+                printf("%c ", choice[i]);
+                if(choice[i] == 'n')
+                    printf("File name is %s \n", argv[i]);
+                if(choice[i] == 'd')
+                    printf("File size is %ld\n", inf.st_size);
+                if(choice[i] == 'h')
+                    printf("Number of hard links is %ld", inf.st_nlink);
+            }
+            printf("\n");
+            // system("clear");
+ 
+        }
+        if(S_ISLNK(inf.st_mode)) {
+            printf("symbolic link\n");
+            printf("Menu for symbolic link \n");
+            printf("1. Link name -n \n");
+            printf("2. Delete link -l \n");
+            printf("3. Size of the link -d \n");
+            printf("4. Size of the target -t \n");
+            printf("5. Access rights -a \n");
+            scanf("%s", choice);
+            for(int i = 1; i < strlen(choice); i++)
+                printf("%c ", choice[i]);
+            printf("\n");
+            // system("clear");
+        }
     }
     return 0;
 }
-
-//C:\Users\Andre\Desktop\UPT\Anul 2 sem 2\OS\OsProject\readme.md
-//C:\Users\Andre\Desktop\UPT\Anul 2 sem 2\SEF\test 1\Capture.JPG
